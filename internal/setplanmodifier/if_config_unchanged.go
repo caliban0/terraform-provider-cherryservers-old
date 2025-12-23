4eb68dcb5701d68acb18df884fc562880cf582dd
Martynas Deveikis <martynasdev@gmail.com>
2025-12-23 15:34:09 +0200

    feat: add setplanmodifier package
    Has custom setplanmodifier mirroring those in stringplanmodifier.

---
+++ internal/setplanmodifier/if_config_unchanged.go
@@ -0,0 +1,55 @@
+package setplanmodifier
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+)
+
+// IfConfigUnchanged compares the states of passed attributes to their configurations
+// and sets UseState to false if, for any of them, the configuration is non-null
+// and different from state. Otherwise, UseState is set to true.
+func IfConfigUnchanged(exp ...path.Expression) UseStateForUnknownIfFunc {
+	return func(ctx context.Context,
+		req planmodifier.SetRequest,
+		resp *UseStateForUnknownIfFuncResponse,
+	) {
+		exps := req.PathExpression.MergeExpressions(exp...)
+
+		resp.UseState = true
+		for _, e := range exps {
+			// Find paths matching the expression in the configuration data.
+			matchedPaths, diags := req.Config.PathMatches(ctx, e)
+
+			resp.Diagnostics.Append(diags...)
+
+			// Collect all errors.
+			if diags.HasError() {
+				continue
+			}
+
+			for _, matchedPath := range matchedPaths {
+				var matchedPathConfigValue attr.Value
+				var matchedPathStateValue attr.Value
+
+				diags = req.Config.GetAttribute(ctx, matchedPath, &matchedPathConfigValue)
+				resp.Diagnostics.Append(diags...)
+				diags = req.State.GetAttribute(ctx, matchedPath, &matchedPathStateValue)
+				resp.Diagnostics.Append(diags...)
+
+				// Collect all errors.
+				if diags.HasError() {
+					continue
+				}
+
+				if !matchedPathConfigValue.IsNull() &&
+					!matchedPathStateValue.Equal(matchedPathConfigValue) {
+					resp.UseState = false
+				}
+			}
+
+		}
+	}
+}
---
+++ internal/setplanmodifier/if_config_unchanged_test.go
@@ -0,0 +1,263 @@
+package setplanmodifier
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+func TestIfConfigUnchangedAtomic(t *testing.T) {
+	t.Parallel()
+
+	const testAttr = "test_attr"
+
+	testSchema := schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			testAttr: schema.StringAttribute{},
+		},
+	}
+
+	testConfig := func(value types.String) tfsdk.Config {
+		tfValue, err := value.ToTerraformValue(context.Background())
+		if err != nil {
+			panic("ToTerraformValue error: " + err.Error())
+		}
+
+		return tfsdk.Config{
+			Schema: testSchema,
+			Raw: tftypes.NewValue(
+				testSchema.Type().TerraformType(context.Background()),
+				map[string]tftypes.Value{
+					testAttr: tfValue,
+				},
+			),
+		}
+	}
+
+	testState := func(value types.String) tfsdk.State {
+		tfValue, err := value.ToTerraformValue(context.Background())
+		if err != nil {
+			panic("ToTerraformValue error: " + err.Error())
+		}
+
+		return tfsdk.State{
+			Schema: testSchema,
+			Raw: tftypes.NewValue(
+				testSchema.Type().TerraformType(context.Background()),
+				map[string]tftypes.Value{
+					testAttr: tfValue,
+				},
+			),
+		}
+	}
+
+	testCases := map[string]struct {
+		req  planmodifier.SetRequest
+		want *UseStateForUnknownIfFuncResponse
+	}{
+		"state-different-from-config": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringValue("a")),
+				State:  testState(types.StringValue("b")),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+		"state-null-config-non-null": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringValue("a")),
+				State:  testState(types.StringNull()),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+		"state-empty-and-different-from-config": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringValue("a")),
+				State:  testState(types.StringValue("")),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+		"config-unknown": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringUnknown()),
+				State:  testState(types.StringValue("b")),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+		"state-same-as-config": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringValue("a")),
+				State:  testState(types.StringValue("a")),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    true,
+				Diagnostics: nil,
+			},
+		},
+		"config-null-and-state-known": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringNull()),
+				State:  testState(types.StringValue("a")),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    true,
+				Diagnostics: nil,
+			},
+		},
+		"state-null-config-null": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(types.StringNull()),
+				State:  testState(types.StringNull()),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    true,
+				Diagnostics: nil,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			f := IfConfigUnchanged(path.MatchRoot(testAttr))
+			got := &UseStateForUnknownIfFuncResponse{}
+			f(context.Background(), testCase.req, got)
+
+			if diff := cmp.Diff(testCase.want, got); diff != "" {
+				t.Errorf("unexpected difference %s", diff)
+			}
+		})
+	}
+}
+
+func TestIfConfigUnchangedComplex(t *testing.T) {
+	t.Parallel()
+
+	const testAttr = "test_attr"
+
+	testSchema := schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			testAttr: schema.SetNestedAttribute{
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						testAttr: schema.StringAttribute{},
+					},
+				},
+			},
+		},
+	}
+
+	testConfig := func(value types.Set) tfsdk.Config {
+		tfValue, err := value.ToTerraformValue(context.Background())
+		if err != nil {
+			panic("ToTerraformValue error: " + err.Error())
+		}
+
+		return tfsdk.Config{
+			Schema: testSchema,
+			Raw: tftypes.NewValue(
+				testSchema.Type().TerraformType(context.Background()),
+				map[string]tftypes.Value{
+					testAttr: tfValue,
+				},
+			),
+		}
+	}
+
+	testState := func(value types.Set) tfsdk.State {
+		tfValue, err := value.ToTerraformValue(context.Background())
+		if err != nil {
+			panic("ToTerraformValue error: " + err.Error())
+		}
+
+		return tfsdk.State{
+			Schema: testSchema,
+			Raw: tftypes.NewValue(
+				testSchema.Type().TerraformType(context.Background()),
+				map[string]tftypes.Value{
+					testAttr: tfValue,
+				},
+			),
+		}
+	}
+
+	objType := types.ObjectNull(map[string]attr.Type{testAttr: types.StringType}).Type(context.Background())
+
+	testCases := map[string]struct {
+		req  planmodifier.SetRequest
+		want *UseStateForUnknownIfFuncResponse
+	}{
+		"child-state-different-from-config": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(
+					types.SetValueMust(
+						objType,
+						[]attr.Value{types.ObjectValueMust(
+							map[string]attr.Type{testAttr: types.StringType},
+							map[string]attr.Value{testAttr: types.StringValue("a")})})),
+				State: testState(
+					types.SetValueMust(
+						objType,
+						[]attr.Value{types.ObjectValueMust(
+							map[string]attr.Type{testAttr: types.StringType},
+							map[string]attr.Value{testAttr: types.StringValue("b")})})),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+		"config-has-no-elements-but-state-does": {
+			req: planmodifier.SetRequest{
+				Config: testConfig(
+					types.SetValueMust(
+						objType,
+						[]attr.Value{})),
+				State: testState(
+					types.SetValueMust(
+						objType,
+						[]attr.Value{types.ObjectValueMust(
+							map[string]attr.Type{testAttr: types.StringType},
+							map[string]attr.Value{testAttr: types.StringValue("b")})})),
+			},
+			want: &UseStateForUnknownIfFuncResponse{
+				UseState:    false,
+				Diagnostics: nil,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			f := IfConfigUnchanged(path.MatchRoot(testAttr))
+			got := &UseStateForUnknownIfFuncResponse{}
+			f(context.Background(), testCase.req, got)
+
+			if diff := cmp.Diff(testCase.want, got); diff != "" {
+				t.Errorf("unexpected difference %s", diff)
+			}
+		})
+	}
+}
